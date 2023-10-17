6bd8b09b2dff5031ff01b45f37d69b2643856425
Dolapo <oyesolabolamide@gmail.com>
2023-10-17 12:30:47 -1100

    commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const menuDrop = document.querySelector(".menu-drop");
 const icon = document.querySelector(".menu-icon");
+const container = document
+	.querySelector(".container")
+	.addEventListener("click", () => {
+		menuDrop.style.height = "0"
+	});
 
 icon.addEventListener("click", (e) => {
 	if (menuDrop.style.height !== "17rem") {
@@ -7,14 +12,15 @@ icon.addEventListener("click", (e) => {
 	} else {
 		menuDrop.style.height = "0";
 	}
+	e.stopPropagation();
 });
 
 const showMoreBtn = document.querySelector(".show-more");
 showMoreBtn.addEventListener("mouseover", () => {
-	showMoreBtn.style.background = '#B88E2F'
-	showMoreBtn.style.color = '#ffffff'
+	showMoreBtn.style.background = "#B88E2F";
+	showMoreBtn.style.color = "#ffffff";
 });
 showMoreBtn.addEventListener("mouseout", () => {
-	showMoreBtn.style.background = 'white'
-    showMoreBtn.style.color = '#B88E2F'
+	showMoreBtn.style.background = "white";
+	showMoreBtn.style.color = "#B88E2F";
 });
